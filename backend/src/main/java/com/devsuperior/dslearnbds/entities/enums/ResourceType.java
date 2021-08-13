ea5629fd312ac8c06fe82f8d3607b7d24dadf577
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-08-13 17:22:29 -0300

    Resource

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/enums/ResourceType.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearnbds.entities.enums;
+
+public enum ResourceType {
+	
+	//Tipos
+	LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+	//posição0        1         2          3
+}
