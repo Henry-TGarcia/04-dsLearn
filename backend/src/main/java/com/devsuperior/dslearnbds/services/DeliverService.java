99a2377af4cb8262dc470c57f4e385d470c5ae6d
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-10-27 10:38:44 -0300

    Preauthorizing methods

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/DeliverService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.entities.Deliver;
+import com.devsuperior.dslearnbds.repositories.DeliverRepositoy;
+
+@Service
+public class DeliverService {
+
+	@Autowired
+	private DeliverRepositoy repository;
+	
+	@Transactional
+	public void saveRevision(Long id, DeliverRevisionDTO dto) {
+		Deliver deliver = repository.getOne(id);
+		deliver.setStatus(dto.getStatus());
+		deliver.setFeedback(dto.getFeedback());
+		deliver.setCorrectCount(dto.getCorrectCount());
+		repository.save(deliver);
+	}
+}
